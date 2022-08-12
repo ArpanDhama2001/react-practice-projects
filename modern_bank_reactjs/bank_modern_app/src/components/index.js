c49adbabc323e881b81f2c0ddb513a6483ce3e2b
Arpan Dhama <arpandhama2001@gmail.com>
2022-08-12 20:55:31 +0530

    bank stats done

---
+++ modern_bank_reactjs/bank_modern_app/src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import FeedbackCard from "./FeedbackCard";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+  Billing,
+  Business,
+  Button,
+  CardDeal,
+  Clients,
+  CTA,
+  FeedbackCard,
+  Footer,
+  GetStarted,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials,
+};
